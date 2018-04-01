3192e6959bdb1c3785035519d1967efd5a4cffac
Alex Pendleton <alexcpendleton@gmail.com>
2018-03-31 17:59:49 -0700

    Add randomness and colors, not super happy with it yet

--- overlap/sketch.js
+++ overlap/sketch.js
@@ -1,11 +1,11 @@
 let mid = {};
 let sides = 6;
-let shapeRadius = 50;
+let baseRadius = 50;
 
 function setup() {
   createCanvas(screen.width, screen.height);
-  //background(bgColor);
-  //colorMode(HSB, 100);
+  const bg = color("#000");
+  //background(bg);
   mid = {
     x: width / 2,
     y: height / 2
@@ -17,20 +17,20 @@ let frameCount = 0;
 let amountDrawn = 0;
 
 function draw() {
-  randomSeed(2734);
+  randomSeed(2734234234);
   // start with tiling polygons within the bounds of the canvas
   // fill it up
   // each one with a random color fill
-  let shapeDiameter = shapeRadius * 2;
+  let shapeDiameter = baseRadius * 2;
   const padding = {
-    x: shapeRadius,
+    x: baseRadius,
     y: shapeDiameter * 2
   };
 
   let beep = 1;
   const step = {
-    x: shapeRadius,
-    y: shapeRadius / 4
+    x: baseRadius,
+    y: baseRadius / 4
   };
 
   let maxShapes = {
@@ -49,67 +49,147 @@ function draw() {
   // \_/ \_/
   //   \_/
   // <-repeat
-  maxShapes.x = ceil((width + padding.x) / shapeRadius);
-  maxShapes.y = ceil((height + padding.y) / shapeRadius);
-  maxShapes.y = 10;
-  maxShapes.x = 10;
+  maxShapes.x = ceil((width + padding.x) / baseRadius);
+  maxShapes.y = ceil((height + padding.y) / baseRadius);
+  //maxShapes.y = 10;
+  //maxShapes.x = 10;
 
   let buffer = {
-    x: 0, //shapeRadius * 2,
-    y: 0 //shapeRadius * 2
-  }
-  translate(buffer.x, buffer.y)
-
+    x: 0, //baseRadius * 2,
+    y: 0 //baseRadius * 2
+  };
+  translate(buffer.x, buffer.y);
+  const toBeDrawn = [];
   for (let j = 0; j < maxShapes.y; j++) {
     for (let i = 0; i < maxShapes.x; i++) {
-
       let isEvenRow = j % 2 == 0;
+
       let fillColor = isEvenRow ? color(255, 0, 0) : color(0, 255, 255);
-
-      fill(fillColor);
-      let x = i * (shapeRadius * 3);
-      let y = j * ((shapeRadius * 1) * .875);
-      //x = round5(x);
-      //y = round5(y);
-      //y = y - (shapeRadius * 1.25)
+      fillColor = nextShapeColor(i, j);
+      //fill(fillColor);
+      //noStroke();
+      let x = i * (baseRadius * 3);
+      let y = j * (baseRadius * 1 * 0.875);
       if (!isEvenRow) {
-        x = x + (shapeRadius * 1.5);
-        y = y - (shapeRadius * 1.75)
-
-      } else {
-        //y = y + (shapeRadius * .125)
-        //y = y - (shapeRadius)
-
-        //x = round5(x);
-        //y = round5(y);
+        x = x + baseRadius * 1.5;
+        y = y - baseRadius * 1.75;
       }
 
-      console.log({
-        i,
-        j,
-        x,
-        y,
-        shapeRadius
-      })
-      polygon(x, y, shapeRadius, 6);
-      if (amountDrawn > 1000) {
-        console.error("AHHHHHH");
-        return;
-      }
+      const actualRadius = fuzz(baseRadius, baseRadius / 2);
+      const sides = 6;
+      const x2 = fuzz(x, 10);
+      const y2 = fuzz(y, 10);
+      const made = makePolygon(x2, y2, actualRadius, sides);
+      made.fillColor = fillColor;
+      toBeDrawn.push(made);
     }
   }
+  drawPolygons(toBeDrawn);
+}
+function drawPolygons(toBeDrawn) {
+  console.log("drawPolygons", toBeDrawn);
+  const copy = toBeDrawn; // toBeDrawn.slice();
+  //shuffleArray(copy);
+  for (let i = 0; i < copy.length; i++) {
+    const element = copy[i];
+    drawPolygon(element);
+  }
+}
+function shuffleArray(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-  //frameCount++;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+function nextShapeColor(x, y) {
+  // let maximumYellow = color(255, 241, 59);
+  // let veryPaleYellow = color(255, 253, 180);
+  // let paleGoldenrod = color(222, 229, 160);
+  // let darkLiverHorses = color(84, 70, 47);
+  // let canary = color(255, 253, 152);
+  // let options = [
+  //   maximumYellow,
+  //   maximumYellow,
+  //   maximumYellow,
+  //   veryPaleYellow,
+  //   paleGoldenrod,
+  //   paleGoldenrod,
+  //   darkLiverHorses,
+  //   canary,
+  //   canary
+  // ];
+  let options = [
+    color("#ed8b00"),
+    color("#ffdd40"),
+    color("#ffdd40"),
+    color("#ffdd40"),
+    color("#fceaa3"),
+    color("#fceaa3"),
+    color("#fceaa3"),
+    color("#f2f7fb")
+  ];
+  let index = random(options.length - 1);
+  index = floor(index);
+  if (options[index] === undefined) {
+    console.log(index);
+    return options[0];
+  }
+  const a = random(225, 255);
+  const i = options[index];
+
+  return color(i.levels[0], i.levels[1], i.levels[2], a);
 }
 
-function round5(x) {
-  return Math.floor(x / 5) * 5;
+function fuzz(i, fuzzFactor) {
+  let r = random(i - fuzzFactor, i + fuzzFactor);
+  return r;
 }
 
 function randomColor() {
   return color(random(255), random(255), random(255));
 }
-
+function makePolygon(x, y, radius, npoints) {
+  let points = [];
+  let angle = TWO_PI / npoints;
+  for (let a = 0; a < TWO_PI; a += angle) {
+    // if (a < 3) {
+    //   stroke(color(255, 0, 0));
+    // } else {
+    //   endShape(CLOSE);
+    //   stroke(color(0, 0, 0));
+    // }
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    //vertex(sx, sy);
+    points.push({ x: sx, y: sy, a: a });
+  }
+  return { points, radius, x, y };
+}
+function drawPolygon(poly) {
+  console.log(poly);
+  fill(poly.fillColor);
+  beginShape();
+  for (let i = 0; i < poly.points.length; i++) {
+    const pt = poly.points[i];
+    strokeWeight(random(0, 0.25));
+    vertex(pt.x, pt.y);
+  }
+  endShape(CLOSE);
+  noFill();
+}
 function polygon(x, y, radius, npoints) {
   let angle = TWO_PI / npoints;
   beginShape();
