5c7b33517b25950b859f2d3e17f06d64817cdb2a
Alex Pendleton <alexcpendleton@gmail.com>
2018-04-15 13:08:28 -0700

    Fix image portions, use newspaper image instead

--- square-collage/sketch.js
+++ square-collage/sketch.js
@@ -1,11 +1,29 @@
 // inspiration https://ericjleffler.carbonmade.com/projects/5796889/14472409
+// image attribution: California Digital Newspaper Collection, Center for Bibliographic Studies and Research, University of California, Riverside, <http://cdnc.ucr.edu>
+
+const shapeRadius = 45;
+let bounds = {
+  w: shapeRadius * 25,
+  h: shapeRadius * 15,
+  start: {
+    x: shapeRadius * 2,
+    y: shapeRadius * 2
+  },
+  imgMins: {
+    x: 100,
+    y: 100
+  }
+};
+let imgLikelihood = 0.2;
+
+let img;
 let mid = {};
-const shapeRadius = 40;
+function preload() {
+  setupImg();
+}
 function setup() {
-  //createCanvas(shapeRadius * 30, shapeRadius * 30);
-  createCanvas(screen.width, screen.height);
+  createCanvas(bounds.w, bounds.h);
   const bg = color("#fff");
-  //background(bg);
   mid = {
     x: width / 2,
     y: height / 2
@@ -14,7 +32,7 @@ function setup() {
   setupSeed();
 }
 function draw() {
-  let rows = height / shapeRadius;
+  let rows = bounds.h / shapeRadius;
 
   for (let i = 0; i < rows; i++) {
     let y = i * shapeRadius * 2;
@@ -22,14 +40,15 @@ function draw() {
   }
 }
 function drawRow(y) {
-  let cols = width / shapeRadius;
+  let cols = bounds.w / shapeRadius;
   for (let i = 0; i < cols; i++) {
     let x = i * shapeRadius * 2;
 
     let pos = { x: fuzz(x, 1, 0.8), y: fuzz(y, 1, 0.8) };
     let r = fuzz(shapeRadius, 1, 1);
-    let p = makePolygon(pos.x, pos.y, r, 4);
-    drawPolygon(p);
+    let p = makePolygon(pos.x + bounds.start.x, pos.y + bounds.start.y, r, 4);
+
+    drawPolygonOrImagePortion(p);
   }
 }
 function fuzz(i, amount, likelihood) {
@@ -55,7 +74,7 @@ function nextShapeColor(x, y) {
   if (options[index] === undefined) {
     return options[options.length - 1];
   }
-  const a = random(225, 255);
+  const a = random(245, 255);
   const i = options[index];
 
   return color(i.levels[0], i.levels[1], i.levels[2], a);
@@ -66,19 +85,6 @@ function randomColor() {
 }
 function makePolygon(x, y, radius, npoints) {
   let points = [];
-  /*
-  let angle = 360 / npoints;
-  console.log(degrees(angle));
-  for (let a = 0; a < 360; a += angle) {
-    console.log(degrees(a));
-    let a2 = radians(a + 45);
-    let sx = x + sin(a2) * radius;
-    let sy = y + cos(a2) * radius;
-    points.push({ x: sx, y: sy, a: a });
-  }
-  */
-  noStroke();
-
   let angle = TWO_PI / npoints;
   for (let a = 0; a < TWO_PI; a += angle) {
     let sx = x + (cos(a) - sin(a)) * radius;
@@ -88,16 +94,40 @@ function makePolygon(x, y, radius, npoints) {
   let fillColor = nextShapeColor(x, y);
   return { points, radius, x, y, fillColor };
 }
+function drawPolygonOrImagePortion(poly) {
+  noStroke();
+  let likelihood = imgLikelihood;
+  if (random() < likelihood) {
+    const w = shapeRadius * 2;
+    const x = poly.x - shapeRadius;
+    const y = poly.y - shapeRadius;
+    drawImagePortion({ img, x, y, w, h: w });
+    return;
+  }
+  drawPolygon(poly);
+}
+function drawImagePortion({ img, x, y, w, h }) {
+  let ix = floor(random(img.width));
+  if (ix < bounds.imgMins.x) {
+    ix += bounds.imgMins.x;
+  }
+  let iy = floor(random(img.height));
+  if (iy < bounds.imgMins.y) {
+    iy += bounds.imgMins.y;
+  }
+  image(img, x, y, w, h, ix, iy, w, h);
+}
+
 function drawPolygon(poly) {
   fill(poly.fillColor);
   push();
-
   beginShape();
   for (let i = 0; i < poly.points.length; i++) {
     const pt = poly.points[i];
     vertex(pt.x, pt.y);
   }
   endShape(CLOSE);
+  pop();
   noFill();
 }
 
@@ -108,6 +138,10 @@ function setupSeed() {
   } else {
     window.seed = ceil(random(0, 1000000));
   }
-  console.log(window.seed);
+  console.log("window.seed", window.seed);
   randomSeed(window.seed);
+}
+
+function setupImg() {
+  img = loadImage("0197.jpg");
 }
