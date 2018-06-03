24b1a21a8b6065dd13abdb49b68d0f633e36ae87
Alex Pendleton <alexcpendleton@gmail.com>
2018-06-02 19:48:16 -0700

    Add helper functions to template

--- _template/sketch.js
+++ _template/sketch.js
@@ -9,10 +9,12 @@ function setup() {
     x: width / 2,
     y: height / 2
   };
-  noLoop();
+  //noLoop();
   setupSeed();
 }
+
 function draw() {}
+
 function setupSeed() {
   const fromLocation = new URL(document.location).searchParams.get("seed");
   if (fromLocation) {
@@ -23,7 +25,35 @@ function setupSeed() {
   console.log(window.seed);
   randomSeed(window.seed);
 }
+
 function randomFromArray(arr) {
   const i = Math.floor(random(0, arr.length));
   return arr[i];
+}
+
+function qp(name, def, customParser) {
+  let value = new URL(document.location).searchParams.get(name);
+  if (value === null || value === undefined || value === "") {
+    value = def;
+  }
+  if (customParser) {
+    value = customParser(value);
+  } else {
+    if (!isNaN(value)) {
+      value = parseFloat(value);
+    }
+  }
+  console.log("qp", name, value, def);
+  return value;
+}
+
+function mouseReleased() {
+  if (mouseReleased.isCurrentlyPaused) {
+    console.log("resumed");
+    mouseReleased.isCurrentlyPaused = false;
+    return loop();
+  }
+  mouseReleased.isCurrentlyPaused = true;
+  noLoop();
+  console.log("paused");
 }
